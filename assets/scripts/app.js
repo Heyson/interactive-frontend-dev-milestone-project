d68359adbc3dee7faaf0e748436c7a093b87003c
Heyson <hs@heysonseverino.com>
2021-01-08 21:05:11 +0000

    Add api key from CLimaCell into const variable

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,6 +7,8 @@ window.addEventListener('load', ()=> {
         (position =>{
             long = position.coords.longitude;
             lat = position.coords.latitude;
+
+            const api = `https://data.climacell.co/v4/locations?apikey=YTqgjLWExGlAk7S6os0GN9Cb377tV7F8`
         });
     }else{
         h1.textContent = "Neep to accept Geo-location for acurate temperature display"

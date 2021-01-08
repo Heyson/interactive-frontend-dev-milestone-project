03399c556fd8bbc7028e3dce765cd5e821303731
Heyson <hs@heysonseverino.com>
2021-01-08 22:07:51 +0000

    Revert loading sample api from OpenWeather

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,7 +8,7 @@ window.addEventListener('load', ()=> {
             lon = position.coords.lon;
             lat = position.coords.lat;
 
-            const api = `https://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=8953477c68d7b2d80e71d5bb16a07eea/${lat},${lon}`;
+            const api = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=8953477c68d7b2d80e71d5bb16a07eea/${lat},${lon}`;
 
             fetch(api)
 .then(response => {

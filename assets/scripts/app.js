d3c0bf5278ff7c479625f633efc1d567e05e16a0
Heyson <hs@heysonseverino.com>
2021-01-10 19:53:47 +0000

    Commit working API call and data calls

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -45,5 +45,29 @@ function showError(error){
 function getWeather(latitude, longitude){
     let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
+    fetch(api)
+    .then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+        weather.description = data.weather[0].description;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+    })
+    .then(function(){
+        displayWeather();
+    });
+
     console.log(api);
+}
+// Dislay Weather To UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="assets/images/icons/${weather.iconId}.png"/>`;
+    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.descriptio;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
+
 }

e72db1fac8320f5ff23af166f6af170a315efa7c
Heyson <hs@heysonseverino.com>
2021-01-10 18:28:44 +0000

    Display weather to UI

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -45,5 +45,26 @@ function showError(error){
 function getWeather(latitude, longitude){
     let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
-    
+    fetch(api)
+    .then(function(response){
+        let data = response.json();
+        return data;
+    })
+    .then(function(data){
+        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+        weather.description = data.weather[0].icon;
+        weather.iconId = data.weather[0].icon;
+        weather.city = data.name;
+        weather.country = data.sys.country;
+    })
+    .then(function(){
+        displayWeather();        
+    });  
+}
+// Display Weather To UI
+function displayWeather(){
+    iconElement.innerHTML = `<img src="assets/images/icons/${weather.iconId}.png"/>`;
+    templateElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+    descElement.innerHTML = weather.description;
+    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }

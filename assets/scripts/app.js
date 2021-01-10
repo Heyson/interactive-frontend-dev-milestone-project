e25aaca0c57c9f104ab5817f8d2302edd38392e5
Heyson <hs@heysonseverino.com>
2021-01-10 17:51:26 +0000

    Set up const vaiables for weather app content

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,24 +1,13 @@
 window.addEventListener('load', ()=> {
-    let lon;
-    let lat;
 
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition
-        (position =>{
-            lon = position.coords.lon;
-            lat = position.coords.lat;
-            const proxy = "cors-anywhere.herokuapp.com/";
-            const api = `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=8953477c68d7b2d80e71d5bb16a07eea`;
+    const proxy = "cors-anywhere.herokuapp.com/";
+    const api = `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=8953477c68d7b2d80e71d5bb16a07eea`;
+    const iconElement = document.querySelector(".weather-icon");
+    const tempElement = document.querySelector(".temperature-value p");
+    const descElement = document.querySelector(".temperature-description p");
+    const locationElement = document.querySelector(".location p");
+    const notificationElement = document.querySelector(".notificatio");
 
-            fetch(api)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                console.log(data);
-            });
-        });
-   
+           
     
-    }
 });

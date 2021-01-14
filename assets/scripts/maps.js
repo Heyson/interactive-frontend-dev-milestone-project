aaf8d2f095f8a920921e0bad1a40c109cf260029
Heyson <hs@heysonseverino.com>
2021-01-14 04:11:23 +0000

    Debug google map, resize to make visible, remove markers

--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -1,14 +1,14 @@
-let map;
-const key = AIzaSyD_9DaD7QnxGgYr5x4M2Z3wmbo-QHo74fY;
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"),{
-        center: {lat: 42.36299, lng: -71.06162},
-        zoom: 10,
-    });
+  let map;
 
+function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: 41.084045, lng: -73.874245 },
+            zoom: 8,
+        });
+
+}
     var trafficLayer = new google.maps.TraffcLayer();
     trafficLayer.setMap(map);
 
    
     
-}

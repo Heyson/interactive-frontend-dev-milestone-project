e2c4615c9887cb89477a416a228f2a4ae0a749c2
Heyson <hs@heysonseverino.com>
2021-01-14 03:57:04 +0000

    Add Google maps traffic layer

--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -2,6 +2,13 @@ let map;
 const key = AIzaSyD_9DaD7QnxGgYr5x4M2Z3wmbo-QHo74fY;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"),{
+        center: {lat: 42.36299, lng: -71.06162},
+        zoom: 10,
+    });
 
-    })
+    var trafficLayer = new google.maps.TraffcLayer();
+    trafficLayer.setMap(map);
+
+   
+    
 }

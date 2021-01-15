99266cbdd6c1f6102f34fe553b28818c4cb95fb1
Heyson <hs@heysonseverino.com>
2021-01-15 12:04:19 +0000

    Format and space code for readibility

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,17 @@
-
+//Used Tutorial Build a Weather App Using Javascript [Beginners] https://www.youtube.com/watch?v=KqZGuzrY9D4
 // Select Elements
-    const proxy = "cors-anywhere.herokuapp.com/";
-    const iconElement = document.querySelector(".weather-icon");
-    const tempElement = document.querySelector(".temperature-value p");
-    const descElement = document.querySelector(".temperature-description p");
-    const locationElement = document.querySelector(".location p");
-    const notificationElement = document.querySelector(".notificatio");
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
+const locationElement = document.querySelector(".location p");
+const notificationElement = document.querySelector(".notificatio");
 
 // App Data
-    const weather = {};
-    
-    weather.temperature = {
-        unit : "celsius"
-    }
+const weather = {};
+
+weather.temperature = {
+    unit: "celsius"
+}
 
 // App Consts and Vars
 const KELVIN = 273;
@@ -20,15 +19,15 @@ const KELVIN = 273;
 const key = "8953477c68d7b2d80e71d5bb16a07eea"
 
 // Check if Browser Supports Geolocation
-if('geolocation' in navigator){
+if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(setPosition, showError);
-}else{
+} else {
     notificationElement.style.display = "block";
     notificationElement.innerHTML = "<p>Please allow Geolocation</p>";
 }
 
 // Set Users Position
-function setPosition(position){
+function setPosition(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
 
@@ -36,35 +35,35 @@ function setPosition(position){
 }
 
 // Show Error when Geolocation is not shown
-function showError(error){
+function showError(error) {
     notificationElement.style.display = "block";
     notificationElement.innerHTML = `<p> ${error.message}</p>`;
 }
 
 // Get Weather From API Provider
-function getWeather(latitude, longitude){
+function getWeather(latitude, longitude) {
     let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
     fetch(api)
-    .then(function(response){
-        let data = response.json();
-        return data;
-    })
-    .then(function(data){
-        weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-        weather.description = data.weather[0].description;
-        weather.iconId = data.weather[0].icon;
-        weather.city = data.name;
-        weather.country = data.sys.country;
-    })
-    .then(function(){
-        displayWeather();
-    });
+        .then(function (response) {
+            let data = response.json();
+            return data;
+        })
+        .then(function (data) {
+            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.description = data.weather[0].description;
+            weather.iconId = data.weather[0].icon;
+            weather.city = data.name;
+            weather.country = data.sys.country;
+        })
+        .then(function () {
+            displayWeather();
+        });
 
     console.log(api);
 }
 // Dislay Weather To UI
-function displayWeather(){
+function displayWeather() {
     iconElement.innerHTML = `<img src="assets/images/icons/${weather.iconId}.png"/>`;
     tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
     descElement.innerHTML = weather.description;
@@ -72,20 +71,20 @@ function displayWeather(){
 }
 
 // C to F conversion
-function celsiusToFahrenheit(temperature){
-    return (temperature * 9/5) + 32;
+function celsiusToFahrenheit(temperature) {
+    return (temperature * 9 / 5) + 32;
 }
 
 // When The User clicks on Temperature Element
-tempElement.addEventListener("click", function(){
-    if(weather.temperature.value === undefined) return;
+tempElement.addEventListener("click", function () {
+    if (weather.temperature.value === undefined) return;
 
-    if(weather.temperature.unit == "celsius"){
+    if (weather.temperature.unit == "celsius") {
         let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
         fahrenheit = Math.floor(fahrenheit);
         tempElement.innerHTML = `${fahrenheit}°<span>F</span>`
         weather.temperature.unit = "fahrenheit";
-    }else{
+    } else {
         tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
         weather.temperature.unit = "celsius";
 
--- assets/scripts/maps.js
+++ assets/scripts/maps.js
@@ -1,59 +1,60 @@
-   
-// Tutorial From Google Maps https://developers.google.com/maps/documentation/javascript/geolocation
+
+// Tutorial From Google Maps https://developers.google.com/maps/documentation/javascript/geolocation and https://developers.google.com/maps/documentation/javascript/examples/place-search
+
 let map, infoWindow, service;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 15,
-  });
-  
-  const trafficLayer = new google.maps.TrafficLayer();
-  trafficLayer.setMap(map);
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 15,
+    });
 
-  const transitLayer = new google.maps.TransitLayer();
-  transitLayer.setMap(map);
+    const trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+
+    const transitLayer = new google.maps.TransitLayer();
+    transitLayer.setMap(map);
 
 
-  infoWindow = new google.maps.InfoWindow();
-  
-  const locationButton = document.createElement("button");
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("You Are Here!.");
-          infoWindow.open(map);
-          trafficLayer.TrafficLayer(map);
-          
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
+    infoWindow = new google.maps.InfoWindow();
+
+    const locationButton = document.createElement("button");
+    locationButton.textContent = "Pan to Current Location";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+    locationButton.addEventListener("click", () => {
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    const pos = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude,
+                    };
+                    infoWindow.setPosition(pos);
+                    infoWindow.setContent("You Are Here!.");
+                    infoWindow.open(map);
+                    trafficLayer.TrafficLayer(map);
+
+                },
+                () => {
+                    handleLocationError(true, infoWindow, map.getCenter());
+                }
+            );
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
         }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
+    });
 }
 
 function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.setMap(map);
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(
+        browserHasGeolocation
+            ? "Error: The Geolocation service failed."
+            : "Error: Your browser doesn't support geolocation."
+    );
+    infoWindow.setMap(map);
 }
 
